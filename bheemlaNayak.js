75fee9e5141d128cdcf720e2e1b40190b7d7537f
srikargunnam <gunnamsri96@gmail.com>
2022-02-18 11:54:14 +0530

    change UI

---
+++ bheemlaNayak.js
@@ -0,0 +1,23 @@
+export const albumName = "Bheemla Nayak";
+
+export const albumCover =
+  "https://naasongs.co/wp-content/uploads/2021/12/Bheemla-Nayak-2021-Adavi-Thalli-Maata-250x250.jpg";
+
+export const data = [
+  {
+    "Bheemla Nayak – Title Song":
+      "https://s10.naasongs.download/tend140nke5jsie/Bheemla%20Nayak%20-%20(2021)/[iSongs.info]%2001%20-%20Bheemla%20Nayak%20-%20Title%20Song.mp3",
+  },
+  {
+    AnthaIstam:
+      "https://s10.naasongs.download/tend140nke5jsie/Bheemla%20Nayak%20-%20(2021)/[iSongs.info]%2002%20-%20AnthaIstam.mp3",
+  },
+  {
+    "Lala Bheemla":
+      "https://s10.naasongs.download/tend140nke5jsie/Bheemla%20Nayak%20-%20(2021)/[iSongs.info]%2003%20-%20Lala%20Bheemla.mp3",
+  },
+  {
+    "Adavi Thalli Maata":
+      "https://s10.naasongs.download/tend140nke5jsie/Bheemla%20Nayak%20-%20(2021)/[iSongs.info]%2004%20-%20Adavi%20Thalli%20Maata.mp3",
+  },
+];
---
+++ pushpa.js
@@ -0,0 +1,27 @@
+export const albumName = "Pushpa: The Rise";
+
+export const albumCover =
+  "https://naasongs.co/wp-content/uploads/2021/11/Pushpa-The-Raise-2021-Eyy-Bidda-Idhi-Naa-Adda-250x250.jpg";
+
+export const data = [
+  {
+    "Pushpa BG Intro":
+      "https://s9.naasongs.download/tejx139kje6nwp/Pushpa%20-%20(2021)/[iSongs.info]%2001%20-%20Pushpa%20BG%20Intro.mp3",
+  },
+  {
+    "Daakko Daakko Meka":
+      "https://s9.naasongs.download/tejx139kje6nwp/Pushpa%20-%20(2021)/[iSongs.info]%2001%20-%20Daakko%20Daakko%20Meka.mp3",
+  },
+  {
+    Srivalli:
+      "https://s9.naasongs.download/tejx139kje6nwp/Pushpa%20-%20(2021)/[iSongs.info]%2002%20-%20Srivalli.mp3",
+  },
+  {
+    "Saami Saami":
+      "https://s9.naasongs.download/tejx139kje6nwp/Pushpa%20-%20(2021)/[iSongs.info]%2003%20-%20Saami%20Saami.mp3",
+  },
+  {
+    "Eyy Bidda Idhi Naa Adda":
+      "https://s9.naasongs.download/tejx139kje6nwp/Pushpa%20-%20(2021)/[iSongs.info]%2004%20-%20Eyy%20Bidda%20Idhi%20Naa%20Adaa.mp3",
+  },
+];
